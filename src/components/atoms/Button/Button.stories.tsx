fe4cfa3d05925dbda16b8e52eadd455fb031ee91
Deepak Prasad <deepakprasad24102002@gmail.com>
2025-08-16 06:14:02 +0530

    Added button action spy, dependabot merge PRs to avoid spam

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
 
 import { Button, type ButtonProps } from './Button';
 
@@ -12,6 +13,7 @@ const meta = {
   },
   args: {
     children: 'Button',
+    onClick: fn(),
   },
   tags: ['autodocs'],
 } satisfies Meta<ButtonProps>;
